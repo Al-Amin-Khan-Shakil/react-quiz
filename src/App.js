1d2c0b2d3ba6f358cf18d3ad8bf1e3f623308ec7
Al-Amin-Khan-Shakil <alaminkhanshakil@gmail.com>
2024-06-09 22:03:25 -0700

    Add loader component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import { useEffect, useReducer } from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 const initialState = {
   questions: [],
-  status: 'loading',
+  status: "loading",
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'dataReceived':
-      return { ...state, questions: action.payload, status: 'ready' };
-    case 'dataFailed':
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
       return {
         ...state,
-        status: 'error',
+        status: "error",
       };
     default:
-      throw new Error('Action unknown');
+      throw new Error("Action unknown");
   }
 }
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => {
-    fetch('http://localhost:8000/questions')
+    fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
-      .catch((err) => dispatch({ type: 'dataFailed' }));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
