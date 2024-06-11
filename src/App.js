c6260d635bd95b558f23cbc4c4a6ea87d939a04e
Al-Amin-Khan-Shakil <alaminkhanshakil@gmail.com>
2024-06-11 15:30:59 -0700

    Set timer for the quiz

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Question from './components/Question';
 import NextButton from './components/NextButton';
 import Progress from './components/Progress';
 import FinishScreen from './components/FinishScreen';
+import Footer from './components/Footer';
+import Timer from './components/Timer';
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
   questions: [],
@@ -16,6 +20,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: 0,
+  secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -28,7 +33,11 @@ function reducer(state, action) {
         status: 'error',
       };
     case 'start':
-      return { ...state, status: 'active' };
+      return {
+        ...state,
+        status: 'active',
+        secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
     case 'newAnswer':
       return {
         ...state,
@@ -54,15 +63,24 @@ function reducer(state, action) {
         status: 'ready',
         highscore: state.highscore,
       };
+    case 'tick':
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? 'finished' : state.status,
+      };
     default:
       throw new Error('Action unknown');
   }
 }
 
 function App() {
-  const [{
-    questions, status, index, answer, points, highscore,
-  }, dispatch] = useReducer(reducer, initialState);
+  const [
+    {
+      questions, status, index, answer, points, highscore, secondsRemaining,
+    },
+    dispatch,
+  ] = useReducer(reducer, initialState);
 
   const maxPossiblePoints = questions.reduce(
     (prev, cur) => prev + cur.points,
@@ -99,12 +117,15 @@ function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton
-              dispatch={dispatch}
-              answer={answer}
-              index={index}
-              numQuestions={questions.length}
-            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={questions.length}
+              />
+            </Footer>
           </>
         )}
         {status === 'finished' && (
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,17 +1,22 @@
 export default function Options({ question, dispatch, answer }) {
   const hasAnswered = answer !== null;
 
+  const getAnswerClass = (index) => {
+    if (hasAnswered) {
+      return index === question.correctOption ? 'correct' : 'wrong';
+    }
+    return '';
+  };
+
   return (
     <div className="options">
       {question.options.map((option, index) => (
         <button
           type="button"
           key={option}
-          className={`btn btn-option ${index === answer ? 'answer' : ''} ${
-            hasAnswered && index === question.correctOption
-              ? 'correct'
-              : 'wrong'
-          }`}
+          className={`btn btn-option ${
+            index === answer ? 'answer' : ''
+          } ${getAnswerClass(index)}`}
           onClick={() => dispatch({ type: 'newAnswer', payload: index })}
           disabled={hasAnswered}
         >
